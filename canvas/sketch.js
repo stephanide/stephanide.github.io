64b8b7e0f331ac4af78ad35c1048fc13d7c3bdb8
stephanide <idestephan@yahoo.de>
2015-11-02 13:19:22 +0000

    update canvas

--- canvas/sketch.js
+++ canvas/sketch.js
@@ -6,6 +6,30 @@ function draw() {
 	// place your drawing code here
 	//this is a note
 	
+	background(200);
+  fill(0);
+
+  var x1 = width/2;
+  var y1 = height/2;
+  var x2 = mouseX;
+  var y2 = mouseY;
+
+  line(x1, y1, x2, y2);
+  ellipse(x1, y1, 7, 7);
+  ellipse(x2, y2, 7, 7);
+
+  // d is the length of the line
+  // the distance from point 1 to point 2.
+  var d = int(dist(x1, y1, x2, y2));
+
+  // Let's write d along the line we are drawing!
+  push();
+  translate( (x1+x2)/2, (y1+y2)/2 );
+  rotate( atan2(y2-y1,x2-x1) );
+  text("no escape", 0, 0);
+  pop();
+  // Fancy!
+	
 	stroke("blue")
 	fill("#3d007e")
 	strokeWeight(10);
