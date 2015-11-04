6dbfd9650b0cdc95b83c268934f3705307e54e98
stephanide <idestephan@yahoo.de>
2015-11-04 17:18:32 +0000

    changing quarters

--- canvas/sketch.js
+++ canvas/sketch.js
@@ -1,3 +1,13 @@
+//picture loading
+
+var img;
+function preload() {
+  img = loadImage("lib/Rad.jpg");
+}
+
+
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
@@ -5,15 +15,51 @@ function setup() {
 function draw() {
 	// place your drawing code here
 	//this is a note
-	
-	background(200);
-  fill(0);
 
-  var x1 = width/2;
-  var y1 = height/2;
+  
+  var x1 = 10;
+  var y1 = 90;
   var x2 = mouseX;
   var y2 = mouseY;
+  
+  
+  
+  
+  
+  //top left quadrant
+  background(200);
+  fill(0);
+  
 
+  
+  if (mouseX > windowWidth/2) {
+    
+    //top right quadrant
+    fill('red');
+    textFont("Georgia");
+    background(255);
+    textSize(100);
+    textStyle(BOLD);
+  }
+  
+  if (mouseY > windowHeight/2) {
+    // bottom left quadrant
+    fill('lime');
+    textFont("Georgia");
+    background(255, 0, 0);
+    textSize(30);
+    textStyle(NORMAL);
+  }
+  
+  if ((mouseX > windowWidth/2) && (mouseY > windowHeight/2)) {
+    //bottom right quadrant
+    fill('blue');
+    textFont("Futura");
+    background(255, 255, 0);
+    textSize(50);
+    textStyle(ITALIC);
+  }
+  
   line(x1, y1, x2, y2);
   ellipse(x1, y1, 7, 7);
   ellipse(x2, y2, 7, 7);
@@ -26,34 +72,30 @@ function draw() {
   push();
   translate( (x1+x2)/2, (y1+y2)/2 );
   rotate( atan2(y2-y1,x2-x1) );
-  text("no escape", 0, 0);
+  text(nfc(d,1,1), 0, -5);
   pop();
   // Fancy!
-	
-	stroke("blue")
-	fill("#3d007e")
-	strokeWeight(10);
-	//noFill();
-	noStroke();
-	ellipse(50, 200, 70, 70);
-	ellipse(width/2,height/2,100,100);
-	stroke ("red");
-	strokeWeight(5);
-	quad(50,50,100,70,250,60,250,300);
-	stroke("green");
-	line(50,200,width/2,50);
-	line(width/2,50,width/2,height/2);
-	stroke("yellow");
-	rect(10,10,70,70,20);
-	stroke("black");
-	triangle(130,50,100,120,100,150);
-	line(10,20,60,60);
-    stroke(255, 102, 0);
-    curve(5, 26, 5, 26, 73, 24, 73, 61);
-    stroke(0);
-    curve(5, 26, 73, 24, 73, 61, 15, 65);
-    stroke(255, 102, 0);
-    curve(73, 24, 73, 61, 15, 65, 15, 65);
 
+  //Picture
+  //image(img, (windowWidth/2)-200, (windowHeight/2)-100, 400, 200);
+
+ //curve
+noFill();
+ curve(windowWidth, 0, windowWidth/2, 200, windowWidth/2, 400, windowWidth, windowHeight);
+ stroke(255, 102, 0);
+
+ 
+ //line
+strokeWeight(100); 
+line(200, 200, 500, 700);
+
+fill(0);
+strokeWeight(0);
+//textSize(20);
+text("DO YOU", (windowWidth/4), (windowHeight/4));
+text("REALLY WANT", ((windowHeight/4)*3),(windowHeight/4)+50);
+text("TO CONCENTRATE", (windowWidth/4), (windowHeight/4)*3);
+text("ON ONE ?", ((windowWidth/4*3)-50), ((windowHeight/4)*3)-50);
+ 
 }
 
